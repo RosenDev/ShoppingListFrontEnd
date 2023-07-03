30dda980f4a46d9224a878a4f1ed87d5f8196494
RosenDev <ros20021999@gmail.com>
2023-07-03 21:56:08 +0300

    code cleanup

--- src/app/product-category/product-category-list/product-category-list.component.ts
+++ src/app/product-category/product-category-list/product-category-list.component.ts
@@ -4,7 +4,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ProductCategoryModel } from 'src/app/model/product-category/product-category.model';
 import { MsalService } from '@azure/msal-angular';
 import { ProductCategoriesService } from 'src/app/service/product-categories.service';
-import { ProductCategoriesService } from 'src/app/service/product-categories.service';
 
 @Component({
   selector: 'app-product-category-list',
